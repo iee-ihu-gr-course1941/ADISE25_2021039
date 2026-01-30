56128e8c94a99a14c477e4ed63405a2fe7014c72
mariaen1 <mariaenechelidou@gmail.com>
2026-01-30 22:01:38 +0200

    Update db2connect.php

--- lib/db2connect.php
+++ lib/db2connect.php
@@ -17,4 +17,6 @@ if(gethostname()=='users.iee.ihu.gr') {
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . 
     $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}?>
+}
+$mysqli->set_charset("utf8mb4");
+?>
